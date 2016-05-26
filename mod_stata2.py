493a3848e5730ce6ee5a9d9309e182147fc0e090
unknown <Sergiy@kompekspe.(none)>
2016-05-25 21:07:49 -0400

    miliseconds bin file added

--- mod_stata2.py
+++ mod_stata2.py
@@ -2,14 +2,16 @@ import BigWorld
 from gui.Scaleform.Battle import Battle
 import datetime
 
+#put compiled shit into C:\Games\World_of_Tanks\res_mods\0.9.14.1\scripts\client\gui\mods
+
 f = open('gamelog.txt', 'a')
-f.write('\n\n-----------\nSTATA mod started '+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')+'\n')
+f.write('\n\n-----------\nSTATA mod started '+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S.%f')+'\n')
 f.close() 
 
 def BF_new_afterCreate(self):
     BF_orig_afterCreate(self)
     f = open('gamelog.txt', 'a')
-    f.write('\n---------Battle starts---------- '+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')+'\n')
+    f.write('\n---------Battle starts---------- '+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S.%f')+'\n')
     f.write( 'I am '+BigWorld.player().name +'\n')
     print BigWorld.player()
     for pl in BigWorld.player().arena.vehicles.values():
@@ -28,8 +30,8 @@ def new_onHealthChanged(self, newHealth, attackerID, attackReasonID):
     EnemyVihInfo = BigWorld.player().arena.vehicles.get(self.id)
     nameWHOdamage = BigWorld.player().arena.vehicles.get(attackerID)
     f = open('gamelog.txt', 'a')
-    f.write (''+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')+ 
-    'DMG  Player '+nameWHOdamage['name'] + ' kicked '+ EnemyVihInfo['name']+' and he has ' +str(newHealth)+' hp left \n')
+    f.write (''+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S.%f')+ 
+    ' Player '+nameWHOdamage['name'] + ' DAMAGED '+ EnemyVihInfo['name']+' and he has ' +str(newHealth)+' hp left \n')
     f.close() 
 old_onHealthChanged = Vehicle.onHealthChanged
 Vehicle.onHealthChanged = new_onHealthChanged
