cdbca65d3746994cefa083285c9025c85ee361ce
unknown <Sergiy@kompekspe.(none)>
2016-05-25 20:56:48 -0400

    wow it is working

--- mod_stata2.py
+++ mod_stata2.py
@@ -3,17 +3,17 @@ from gui.Scaleform.Battle import Battle
 import datetime
 
 f = open('gamelog.txt', 'a')
-print 'STATA mod started '+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
+f.write('\n\n-----------\nSTATA mod started '+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')+'\n')
 f.close() 
 
 def BF_new_afterCreate(self):
     BF_orig_afterCreate(self)
     f = open('gamelog.txt', 'a')
-    f.write('Battle starts '+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')+'\n')
+    f.write('\n---------Battle starts---------- '+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')+'\n')
     f.write( 'I am '+BigWorld.player().name +'\n')
     print BigWorld.player()
     for pl in BigWorld.player().arena.vehicles.values():
-        f.write(pl['name']+' '+pl['accountDBID']+'\n')
+        f.write(pl['name']+' '+str(pl['accountDBID'])+' team '+str(pl['team'])+'\n')
     f.close() 
 
 BF_orig_afterCreate = Battle.afterCreate
@@ -26,10 +26,10 @@ old_onHealthChanged = Vehicle.onHealthChanged
 def new_onHealthChanged(self, newHealth, attackerID, attackReasonID):
     old_onHealthChanged(self, newHealth, attackerID, attackReasonID)
     EnemyVihInfo = BigWorld.player().arena.vehicles.get(self.id)
-    nameWHOdamage = g_sessionProvider.getCtx().getFullPlayerName(attackerID, showClan=False)
+    nameWHOdamage = BigWorld.player().arena.vehicles.get(attackerID)
     f = open('gamelog.txt', 'a')
-    f.write ('Damaga '+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')+ 
-    ' Player '+nameWHOdamage + ' kicked '+ EnemyVihInfo['name']+' and he has ' +str(newHealth)+' hp left')
+    f.write (''+datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')+ 
+    'DMG  Player '+nameWHOdamage['name'] + ' kicked '+ EnemyVihInfo['name']+' and he has ' +str(newHealth)+' hp left \n')
     f.close() 
 old_onHealthChanged = Vehicle.onHealthChanged
 Vehicle.onHealthChanged = new_onHealthChanged
